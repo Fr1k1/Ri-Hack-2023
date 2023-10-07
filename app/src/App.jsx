fa44410f86129cbf069b99c8fc90d03be5457994
Fr1k1 <martin.friscic6@gmail.com>
2023-10-07 15:47:46 +0200

    [FRONTEND] Add login form design

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "./Components/templ/MainLayout";
-import Input from "./Components/atoms/Input";
+import Login from "./Components/pages/Login/Login";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route
-            path="/login"
-            element={<Input placeholder={"plc"} label={"Lbl"} />}
-          />
           <Route path="/krec" element={<h1>something</h1>} />
         </Route>
+
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
---
+++ app/src/Components/pages/Login/Login.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Login.scss";
+import Runner from "../../../assets/jogging.png";
+import Input from "../../atoms/Input";
+import Button from "../../atoms/Button";
+
+const Login = () => {
+  return (
+    <div className="login-page-wrapper">
+      <img src={Runner} alt="" />
+      <h2>Welcome to MoveMent</h2>
+
+      <div className="login-form-wrapper">
+        <form action="" id="login-form">
+          <Input placeholder={"Username"} label={"Username"} />
+          <Input placeholder={"Password"} label={"Password"} />
+          <div className="forgot-password-wrapper">
+            <h4>Forgot password?</h4>
+          </div>
+          <Button>Login</Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;

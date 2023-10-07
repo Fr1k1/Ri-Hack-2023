ace9476d565a487dd9c862d7c1e74df9b1c6c6fe
Ana Skarica <ana.skarica.as@gmail.com>
2023-10-07 14:03:03 +0200

    Add font, images, task card component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,6 +3,7 @@ import Input from "./Components/Input/Input";
 import { Route, Routes } from "react-router-dom";
 import Button from "./Components/Button/Button";
 import MobileNavbar from "./Components/MobileNabvar/MobileNavbar";
+import TaskCard from "./Components/TaskCard/TaskCard";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
           path="/login"
           element={<Input placeholder={"plc"} label={"Lbl"} />}
         ></Route>
-
+        <Route path="/tasks" element={<TaskCard />}></Route>
         <Route path="/" element={<MobileNavbar />}></Route>
       </Routes>
     </>

f22809c76f85b34e6558a4c88d3239fbc901bd4e
Ana Skarica <ana.skarica.as@gmail.com>
2023-10-07 18:58:44 +0200

    Add profile, disabled input

--- app/src/Components/atoms/Input/Input.jsx
+++ app/src/Components/atoms/Input/Input.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Input.scss";
 
-const Input = ({ placeholder, label, type = "text" }) => {
+const Input = ({
+  placeholder,
+  label,
+  type = "text",
+  defaultValue,
+  isDisabledDefault,
+}) => {
+  const [value, setValue] = useState(defaultValue);
+  const [isDisabled, setIsDisabled] = useState(isDisabledDefault);
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log(isDisabledDefault + " isDisabledDefault");
+    setIsDisabled(isDisabledDefault);
+  }, [isDisabledDefault]);
+
   return (
     <div className="input-wrapper">
       {label && <label className="label-main">{label}</label>}
-      <input type={type} placeholder={placeholder} className="input-main" />
+      <input
+        type={type}
+        placeholder={placeholder}
+        className={`input-main ${isDisabled ? "disabled" : ""}`}
+        value={value}
+        disabled={isDisabled}
+        onChange={handleChange}
+      />
     </div>
   );
 };

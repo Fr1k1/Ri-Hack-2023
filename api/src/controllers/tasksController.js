e3ac28b57611cf80509a85aa8abfb1a11a1eead7
ttomasicc <tin.tomasic74@gmail.com>
2023-10-07 19:40:31 +0200

    TT - fixed task creation

--- api/src/controllers/tasksController.js
+++ api/src/controllers/tasksController.js
@@ -6,19 +6,21 @@ export const getAllTasks = factory.getAll('task')
 export const getTask = factory.getOne('task')
 
 export const createTask = async (req, res, next) => {
-    const { statusId, difficultyId, name, reward, description, groupSize, lat, lng, startDate, endDate } = req.body
+  const task = extractTaskFromReqBody(req)
 
-    if (!statusId || !difficultyId || !name || !reward || !description || !groupSize ||
-        !lat || !lng || !startDate || !endDate)
-        return next(new AppError('Insufficient parameters.', 422))
+  if (!task.statusId || !task.difficultyId || !task.name ||
+    !task.reward || !task.description || !task.groupSize ||
+    !task.lat || !task.lng || !task.startDate || !task.endDate) {
+    return next(new AppError('Missing task info.', 422))
+  }
 
-    const { lastID } = await exec('INSERT INTO task(name, reward, description, group_size, lat, lng, start_date, end_date, status_id, difficulty_id ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', [name, reward, description, groupSize, lat, lng, startDate, endDate, statusId, difficultyId])
-    const newTask = await exec("SELECT * FROM task WHERE id = ?;", [lastID])
+  const { lastID } = await exec('INSERT INTO task(name, reward, description, group_size, lat, lng, start_date, end_date, status_id, difficulty_id ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', [task.name, task.reward, task.description, task.groupSize, task.lat, task.lng, task.startDate, task.endDate, task.statusId, task.difficultyId])
+  const newTask = (await exec('SELECT * FROM task WHERE id = ?;', [lastID]))[0]
 
-    res.status(201).json({
-        status: 'success',
-        data: { newTask: newTask }
-    })
+  res.status(201).json({
+    status: 'success',
+    data: { task: newTask }
+  })
 }
 
 // export const addUserToTask = (req, res, next) => {
@@ -29,27 +31,29 @@ export const createTask = async (req, res, next) => {
 
 // }
 
-// // export const updateTask = (req, res, next) => {
-// //     const { difficultyId, name, reward, description, groupSize, lat, lng, startDate, endDate } = req.body
+export const updateTask = (req, res, next) => {
+  const task = extractTaskFromReqBody(req)
 
-
-// //     const numberOfGroupSize = 
-
-// // }
+}
 
 export const deleteTask = async (req, res, next) => {
-    const { id } = req.params
+  const { id } = req.params
 
-    const fetchedTask = await exec("SELECT * FROM task WHERE id = ?;", [id])
+  const fetchedTask = await exec('SELECT * FROM task WHERE id = ?;', [id])
 
-    if (fetchedTask.length == 0)
-        return next(new AppError("Task doesn't exist.", 404))
+  if (fetchedTask.length === 0)
+    return next(new AppError('Task doesn\'t exist.', 404))
 
-    await exec("DELETE FROM task WHERE id = ?;", [id])
+  await exec('DELETE FROM task WHERE id = ?;', [id])
 
-    res.status(200).json({
-        status: 'success',
-        data: { deletedTask: fetchedTask }
-    })
+  res.status(200).json({
+    status: 'success',
+    data: { deletedTask: fetchedTask }
+  })
 }
 export const getTasksWithin = (req, res) => 1
+
+const extractTaskFromReqBody = (req) => {
+  const { statusId, difficultyId, name, reward, description, groupSize, lat, lng, startDate, endDate } = req.body
+  return { statusId, difficultyId, name, reward, description, groupSize, lat, lng, startDate, endDate }
+}

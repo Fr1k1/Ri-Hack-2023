95a65122359dd3092284fa2bcc2285c877950747
Fr1k1 <martin.friscic6@gmail.com>
2023-10-07 18:51:57 +0200

    [FRONTEND] Connect login to API

--- app/src/Components/pages/Login/Login.jsx
+++ app/src/Components/pages/Login/Login.jsx
@@ -1,24 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.scss";
 import Runner from "../../../assets/jogging.png";
 import Input from "../../atoms/Input";
 import Button from "../../atoms/Button";
 import { Link } from "react-router-dom";
+import { loginUser } from "../../../api/api";
 
 const Login = () => {
+  const [user, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (name, value) => {
+    setUserData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+
+    console.log(value);
+  };
+
+  const handleSubmit = async (e) => {
+    //console.log("Postavljeni podaci:", staffData);
+    e.preventDefault();
+
+    //const isValid = validateForm();
+
+    // if (isValid) {
+    try {
+      await loginUser(user);
+      console.log("Uspjesno prijavljen");
+      //notifySuccess("Uspješno dodano osoblje");
+      //console.log("Tenant submitted successfully");
+    } catch (error) {
+      //console.log("Error submitting tenant:", error);
+      //notifyFailure();
+    }
+    //}
+  };
+
+  const handleUserEmailChange = (e) => {
+    handleInputChange("email", e.target.value);
+  };
+
+  const handleUserPasswordChange = (e) => {
+    handleInputChange("password", e.target.value);
+  };
   return (
     <div className="login-page-wrapper">
       <img src={Runner} alt="" />
       <h2>Welcome to MoveMent</h2>
 
       <div className="login-form-wrapper">
-        <form action="" id="login-form">
-          <Input placeholder={"Username"} label={"Username"} />
-          <Input placeholder={"Password"} label={"Password"} />
+        <form action="" id="login-form" onSubmit={handleSubmit}>
+          <Input
+            placeholder={"Email"}
+            label={"Email"}
+            name="email"
+            value={user?.email}
+            onChange={handleUserEmailChange}
+          />
+          <Input
+            placeholder={"Password"}
+            label={"Password"}
+            name="password"
+            value={user?.password}
+            onChange={handleUserPasswordChange}
+          />
           <div className="forgot-password-wrapper">
             <h4>Forgot password?</h4>
           </div>
-          <Button>Login</Button>
+          <Button type="submit">Login</Button>
         </form>
 
         <div className="no-account-wrapper">
--- app/src/api/api.js
+++ app/src/api/api.js
@@ -1,17 +1,37 @@
 let api = "http://localhost:3000/api";
 
-export async function addNewUser(staffData) {
+export async function addNewUser(userData) {
   const response = await fetch(`${api}/v1/users/signup`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(staffData),
+    body: JSON.stringify(userData),
   });
 
   const responseData = await response.json();
 
   console.log("Uspjesna registracija");
+
+  if (!response.ok) {
+    throw new Error("Failed to add new staff");
+  }
+
+  return responseData;
+}
+
+export async function loginUser(userData) {
+  const response = await fetch(`${api}/v1/users/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+
+  const responseData = await response.json();
+
+  console.log("Uspjesna prijava");
 
   if (!response.ok) {
     throw new Error("Failed to add new staff");

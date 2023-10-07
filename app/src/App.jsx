bea2c44a761cde9c1d28c98847c8eee303ed5c97
Fr1k1 <martin.friscic6@gmail.com>
2023-10-07 14:48:40 +0200

    [FRONTEND] Add main layout

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
-import Input from "./Components/Input/Input";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Button from "./Components/Button/Button";
-import MobileNavbar from "./Components/MobileNabvar/MobileNavbar";
+import MainLayout from "./Components/MainLayout/MainLayout";
+import Input from "./Components/Input/Input";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route
-          path="/login"
-          element={<Input placeholder={"plc"} label={"Lbl"} />}
-        ></Route>
-
-        <Route path="/" element={<MobileNavbar />}></Route>
+        <Route path="/" element={<MainLayout />}>
+          <Route
+            path="/login"
+            element={<Input placeholder={"plc"} label={"Lbl"} />}
+          />
+          <Route path="/krec" element={<h1>something</h1>} />
+        </Route>
       </Routes>
     </>
   );

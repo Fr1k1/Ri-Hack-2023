94beddb58cf1a2d1697ee81359759dcc884d96c4
Fr1k1 <martin.friscic6@gmail.com>
2023-10-07 12:31:16 +0200

    [FRONTEND] Add react router

--- frontend/ri-hack-2023/src/App.jsx
+++ frontend/ri-hack-2023/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
 import Input from "./Components/Input/Input";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Input placeholder={"plcholder"} label={"Ovo je label"} />
+      <Routes>
+        <Route
+          path="/login"
+          element={<Input placeholder={"plc"} label={"Lbl"} />}
+        ></Route>
+      </Routes>
     </>
   );
 }

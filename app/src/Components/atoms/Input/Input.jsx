9197cc5ce0abbe6ddbcbb5119559a6cdc32afb6e
Ana Skarica <ana.skarica.as@gmail.com>
2023-10-07 19:09:29 +0200

    Add disabled input field

--- app/src/Components/atoms/Input/Input.jsx
+++ app/src/Components/atoms/Input/Input.jsx
@@ -1,15 +1,28 @@
 import React, { useEffect, useState } from "react";
 import "./Input.scss";
 
-const Input = ({ placeholder, label, type = "text", onChange }) => {
+const Input = ({
+  placeholder,
+  label,
+  type = "text",
+  onChange,
+  isDisabledDefault,
+  defaultValue,
+}) => {
+  const [isDisabled, setIsDisabled] = useState(isDisabledDefault);
+  useEffect(() => {
+    console.log(isDisabledDefault + " isDisabledDefault");
+    setIsDisabled(isDisabledDefault);
+  }, [isDisabledDefault]);
   return (
     <div className="input-wrapper">
       {label && <label className="label-main">{label}</label>}
       <input
         type={type}
         placeholder={placeholder}
-        className="input-main"
+        className={`input-main ${isDisabled ? "disabled" : ""}`}
         onChange={onChange}
+        disabled={isDisabled}
       />
     </div>
   );
